c50c62d31d87cfb31ae7d7f9aef3e1a2bda9d331
Flyli0 <kirillrozkov653@gmail.com>
2025-11-16 20:47:11 +0500

    Btester must be from lab 11!!!!!!!!!!!!!!!!!!!

---
+++ Lab10/Btester.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> adj(n + 1);
+    vector<int> indegree(n + 1, 0);
+
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        indegree[v]++;
+    }
+
+    queue<int> q;
+    vector<int> order;
+
+    for (int i = 1; i <= n; i++) {
+        if (indegree[i] == 0)
+            q.push(i);
+    }
+
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        order.push_back(u);
+
+        for (int v : adj[u]) {
+            indegree[v]--;
+            if (indegree[v] == 0)
+                q.push(v);
+        }
+    }
+
+    if ((int)order.size() != n) {
+        cout << "Impossible\n";
+        return 0;
+    }
+
+    cout << "Possible\n";
+    for (int x : order)
+        cout << x << " ";
+    cout << "\n";
+
+    return 0;
+}
